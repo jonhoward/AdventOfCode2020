621982a3709adbfcd27e2dc0917ac751ac757d4c
Jonathan Howard <jchoward@gmail.com>
2020-12-06 00:57:41 -0500

    Added day 6.

---
+++ Day06/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(".\\input.txt");
+
+            string anyoneQuestions = "";
+            IEnumerable<char> everyoneQuestions = null;
+            int partOneSum = 0;
+            int partTwoSum = 0;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    partOneSum += anyoneQuestions.Trim().Distinct().Count();
+                    partTwoSum += everyoneQuestions.Count();
+                    anyoneQuestions = "";
+                    everyoneQuestions = null;
+                }
+                else
+                {
+                    anyoneQuestions += line;
+
+                    if (everyoneQuestions == null)
+                    {
+                        everyoneQuestions = line.ToList();
+                    }
+                    else
+                    {
+                        everyoneQuestions = everyoneQuestions.Intersect(line.ToList());
+                    }
+                }
+            }
+
+            partOneSum += anyoneQuestions.Trim().Distinct().Count();
+            partTwoSum += everyoneQuestions.Count();
+
+            Console.WriteLine(partOneSum);
+            Console.WriteLine(partTwoSum);
+
+            Console.Read();
+        }
+    }
+}
