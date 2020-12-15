eab62a1f687e79b5f656e88c55a1ec68d7892c40
Jonathan Howard <jchoward@gmail.com>
2020-12-14 19:25:36 -0500

    Add days 13 and 14 (all caught up, for now...)

---
+++ Day13/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(".\\input.txt");
+            int earliestDeparture = int.Parse(lines[0]);
+            var buses = new Dictionary<int, int>();
+            var busList = lines[1].Split(',');
+
+            // Part 1
+            // I'm making an assumption here that no buses leave AT the earliest available time
+            // because 0 would be a boring answer to enter.
+            busList.Where(b => b != "x")
+                .Select(b => int.Parse(b))
+                .ToList()
+                .ForEach(b => buses.Add(b, b* (earliestDeparture / b + 1)));
+
+            int firstDeparture = buses.Min(b => b.Value);
+            int bus = buses.First(b => b.Value == firstDeparture).Key;
+
+            Console.WriteLine(bus * (firstDeparture - earliestDeparture));
+
+            // Part 2
+            var offsets = new Dictionary<int, int>();
+            busList.Where(b => b != "x")
+                .ToList()
+                .ForEach(b => offsets.Add(int.Parse(b), Array.IndexOf(busList, b)));
+
+            long testVal = offsets.First().Key;
+            long step = testVal;
+
+            foreach (var pair in offsets.Skip(1))
+            {
+                while ((testVal + pair.Value) % pair.Key != 0)
+                {
+                    testVal += step;
+                }
+
+                step *= pair.Key;
+            }
+
+            Console.WriteLine(testVal);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(".\\input.txt");
+            var mem = new Dictionary<long, long>();
+            string mask = "";
+
+            // Part 1
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = line.Substring(7);
+                }
+                else
+                {
+                    long address = long.Parse(line.Substring(4, line.IndexOf(']') - 4));
+                    long value = long.Parse(line.Substring(line.IndexOf('=') + 2));
+                    mem[address] = GetMaskedValue(mask, value);
+                }
+            }
+
+            Console.WriteLine(mem.Values.Sum());
+
+            mask = "";
+            mem.Clear();
+
+            // Part 2
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    mask = line.Substring(7);
+                }
+                else
+                {
+                    long address = long.Parse(line.Substring(4, line.IndexOf(']') - 4));
+                    long value = long.Parse(line.Substring(line.IndexOf('=') + 2));
+                    GetFloatingMaskedValues(mask, address).ForEach(a => mem[a] = value);
+                }
+            }
+
+            Console.WriteLine(mem.Values.Sum());
+
+            Console.ReadKey();
+        }
+
+        private static long GetMaskedValue(string mask, long value)
+        {
+            char[] result = Convert.ToString(value, 2).PadLeft(36, '0').ToArray();
+
+            for (int i = 0; i < mask.Length; i ++)
+            {
+                result[i] = mask[i] == 'X' ? result[i] : mask[i];
+            }
+
+            return Convert.ToInt64(new string(result), 2);
+        }
+
+        private static List<long> GetFloatingMaskedValues(string mask, long value)
+        {
+            char[] resultTemplate = Convert.ToString(value, 2).PadLeft(36, '0').ToArray();
+            var resultStrings = new List<string>();
+            resultStrings.Add("");
+
+            for (int i = 0; i < mask.Length; i ++)
+            {
+                if (mask[i] == '0')
+                {
+                    resultStrings = resultStrings.Select(s => s += resultTemplate[i]).ToList();
+                }
+                else if (mask[i] == '1')
+                {
+                    resultStrings = resultStrings.Select(s => s += '1').ToList();
+                }
+                else
+                {
+                    resultStrings = resultStrings.Select(s => s += '0').ToList();
+                    resultStrings.AddRange(resultStrings.Select(s => s.Substring(0, i) + '1').ToList());
+                }
+            }
+
+            return resultStrings.Select(s => Convert.ToInt64(s, 2)).ToList();
+        }
+    }
+}
